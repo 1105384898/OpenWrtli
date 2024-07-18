db8e09067cd95b89295c197dcdea47638cf7d84e
yangyang97 <46973226+1105384898@users.noreply.github.com>
2024-07-18 16:02:57 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -4,7 +4,7 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
-git clone https://github.com/jerrykuku/luci-theme-argon /package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
 src-git istore https://github.com/linkease/istore;main
 src-git nas https://github.com/linkease/nas-packages.git;master
@@ -12,6 +12,7 @@ src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main
 
 src-git gowebdav https://github.com/immortalwrt-collections/openwrt-gowebdav.git;main
 
+./scripts/feeds update -a && ./scripts/feeds install -a
 # 后台IP设置
 export Ipv4_ipaddr="192.168.5.1"            # 修改openwrt后台地址(填0为关闭)
 export Netmask_netm="255.255.255.0"         # IPv4 子网掩码（默认：255.255.255.0）(填0为不作修改)
